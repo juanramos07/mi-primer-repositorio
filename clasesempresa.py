be20c32330721e2ae1d053ececd5c6114f550bb3
juanramos07 <L18TE0365@teziutlan.tecnm.mx>
2021-11-13 17:20:21 -0600

    se agrego la clase Gerente

--- clasesempresa.py
+++ clasesempresa.py
@@ -1,4 +1,32 @@
-class persona:
+
+class Persona:
+    def __init__(self,nombre,edad,estatura,peso,genero = "x"):
+        self.nombre = nombre
+        self.genero = genero
+        self.edad = edad
+        self.estatura = estatura
+        self.peso = peso
+
+    def describir(self):
+        print(" ")
+        print("nombre: ",self.nombre)
+        print("Genero : ",self.genero)
+        print("Edad: ",self.edad)
+        print("estatura: ",self.estatura)
+        print("peso: ",self.peso)
+
+class Gerente(persona):
+    def __init__(self,nombre,edad,estatura,peso,genero,sueldo):
+        super().__init__(nombre, edad, estatura, peso, genero)
+        self.sueldo = sueldo
+
+    def trabajar(self):
+        print("trabajando")
+
+    def principiante(self, other):
+        other.__class__ = empleado
+
+class Empleado(Gerente):
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
@@ -8,24 +36,5 @@ class persona:
 
 
 
-class empleado(persona):
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
 
-    def trabajar(self):
-        print("trabajando")
-
-    def principiante(self, other):
-        other.__class__ = empleado
-
-
-juan = persona("juan", 25)
-juan.viajando()
-print(type(juan))
-jose = empleado("jose", 23)
-jose.trabajar()
-
-#jose.principiante(juan)
-#print(type(juan))
 
